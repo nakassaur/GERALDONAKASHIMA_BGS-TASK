85bd528a09038499b5f6122a017802c7c5d818c3
nakassaur <38019545+nakassaur@users.noreply.github.com>
2024-05-28 01:36:09 -0300

    Basic Player Movement, In-Game Menu Draft

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] PlayerInput _playerInput;
+    [SerializeField] Rigidbody2D _rb2d;
+
+    InputAction _moveAction;
+    [SerializeField] Vector2 _move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _moveAction = _playerInput.actions["Move"];        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _move = _moveAction.ReadValue<Vector2>();
+        _rb2d.velocity = _move * 5;
+    }
+}
